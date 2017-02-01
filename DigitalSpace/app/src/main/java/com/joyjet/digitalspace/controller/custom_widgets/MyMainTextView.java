a30c5ba96b96039d8ca33c3af4ea1d16c5d4bec4
César Rodrigues <rasec1287@hotmail.com>
2017-02-01 15:51:02 -0300

    added class for the custom textview

--- DigitalSpace/app/src/main/java/com/joyjet/digitalspace/controller/custom_widgets/MyMainTextView.java
+++ DigitalSpace/app/src/main/java/com/joyjet/digitalspace/controller/custom_widgets/MyMainTextView.java
@@ -9,6 +9,6 @@ public class MyMainTextView extends TextView {
 
     public MyMainTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        this.setTypeface(Typeface.createFromAsset(context.getAssets(),"fonts/PoiretOne-Regular.ttf"));
+        this.setTypeface(Typeface.createFromAsset(context.getAssets(),"fonts/Montserrat-Bold.otf"));
     }
 }
