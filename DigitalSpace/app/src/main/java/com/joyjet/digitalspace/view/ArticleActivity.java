e5ee1096f30d628581baf220fabfbcd2a3975fe5
César Rodrigues <rasec1287@hotmail.com>
2017-02-03 17:35:45 -0300

    Article Activity updated

--- DigitalSpace/app/src/main/java/com/joyjet/digitalspace/view/ArticleActivity.java
+++ DigitalSpace/app/src/main/java/com/joyjet/digitalspace/view/ArticleActivity.java
@@ -1,7 +1,6 @@
 package com.joyjet.digitalspace.view;
 
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -31,6 +30,48 @@ public class ArticleActivity extends AppCompatActivity {
         setupUI();
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id){
+            case android.R.id.home:
+                finish();
+                break;
+            case R.id.action_fav:
+                if(dao.isFav(article.getCategory(), article.getTitle())){
+                    dao.delete(article.getCategory(), article.getTitle());
+                    item.setIcon(R.drawable.ic_heart_outline_white_48dp);
+                    Toast.makeText(this, R.string.message_article_removed_favorities, Toast.LENGTH_SHORT).show();
+                }else {
+                    dao.insertFav(article);
+                    dao.close();
+                    item.setIcon(R.drawable.ic_heart_white_48dp);
+                    Toast.makeText(this, R.string.message_article_added_favorities, Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_fav, menu);
+
+        if(article!=null) {
+            if (dao.isFav(article.getCategory(), article.getTitle())) {
+                menu.getItem(0).setIcon(R.drawable.ic_heart_white_48dp);
+            } else {
+                menu.getItem(0).setIcon(R.drawable.ic_heart_outline_white_48dp);
+            }
+        }
+
+        return true;
+    }
+
     private void setupUI() {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -53,47 +94,5 @@ public class ArticleActivity extends AppCompatActivity {
         tv_category.setText(article.getCategory());
         TextView tv_text = (TextView) findViewById(R.id.article_details_text);
         tv_text.setText(article.getDescription());
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        switch (id){
-            case android.R.id.home:
-                finish();
-                break;
-            case R.id.action_fav:
-                if(dao.isFav(article.getCategory(), article.getTitle())){
-                    dao.delete(article.getCategory(), article.getTitle());
-                    item.setIcon(R.drawable.ic_heart_outline_white_48dp);
-                    Toast.makeText(this, "Article removed to favorites", Toast.LENGTH_SHORT).show();
-                }else {
-                    dao.insertFav(article);
-                    dao.close();
-                    item.setIcon(R.drawable.ic_heart_white_48dp);
-                    Toast.makeText(this, "Article added to favorites", Toast.LENGTH_SHORT).show();
-                }
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fav, menu);
-
-        if(article!=null) {
-            if (dao.isFav(article.getCategory(), article.getTitle())) {
-                menu.getItem(0).setIcon(R.drawable.ic_heart_white_48dp);
-            } else {
-                menu.getItem(0).setIcon(R.drawable.ic_heart_outline_white_48dp);
-            }
-        }
-
-        return true;
     }
 }
